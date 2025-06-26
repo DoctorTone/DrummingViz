2ab8bd84983b55d42e8ca808d7d4a8d27d642b41
DoctorTone <tony@drt-software.com>
2025-06-26 18:46:37 +0100

    Added groove text and adjusted timeline length

--- src/UI/Score.tsx
+++ src/UI/Score.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import Typography from "@mui/material/Typography";
 
 const Score = () => {
   return (
     <div id="score" className="panel">
+      <Typography variant="h6" sx={{ textAlign: "center" }}>
+        Groove 1
+      </Typography>
       <img src="./images/drumScore1.png" className="w-100" />
     </div>
   );
