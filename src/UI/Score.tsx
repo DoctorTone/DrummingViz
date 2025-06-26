e87178c44aa7fdeac634076f3e66e605375b84dc
DoctorTone <tony@drt-software.com>
2025-06-26 18:08:24 +0100

    Make drum score transparent and sharpen

--- src/UI/Score.tsx
+++ src/UI/Score.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Score = () => {
   return (
     <div id="score" className="panel">
-      <img src="./images/drumScore1.jpg" className="w-100" />
+      <img src="./images/drumScore1.png" className="w-100" />
     </div>
   );
 };
