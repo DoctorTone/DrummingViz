b2c3d8bb5e5f24c7a16101bac90aab0c634aa857
DoctorTone <tony@drt-software.com>
2025-06-26 17:36:25 +0100

    Added animated timeline

--- src/UI/TimeLine.tsx
+++ src/UI/TimeLine.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "@mui/material/Button";
 
 const TimeLine = () => {
-  const playTune = () => {
-    const elem = document.getElementById("bar");
-    elem!.style.transform = "translate(30px)";
+  const requestRef = useRef(0);
+  const elemRef = useRef<HTMLDivElement>(null);
+  const positionRef = useRef(31);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const togglePlay = () => {
+    setIsAnimating((prev) => !prev);
   };
+
+  const animate = () => {
+    elemRef.current!.style.left = `${positionRef.current++}%`;
+    if (positionRef.current >= 68) {
+      positionRef.current = 31;
+    }
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    if (isAnimating) {
+      requestRef.current = requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(requestRef.current);
+    }
+
+    // Clean up on unmount
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isAnimating]);
 
   return (
     <>
-      <div id="timeLine" className="panel">
-        <div id="bar"></div>
-      </div>
+      <div ref={elemRef} id="timeLine" className="panel"></div>
       <div id="play" className="panel">
-        <Button variant="contained" onClick={playTune}>
+        <Button variant="contained" onClick={togglePlay}>
           Play
         </Button>
       </div>
